012a557948d54ccb24c5c46a628ce64b8a4df866
lgem <23lgem@gmail.com>
2020-05-12 20:52:09 -0300

    added dust_2

--- js/interactive.js
+++ js/interactive.js
@@ -1,16 +1,16 @@
 function change(obj) {
     detail = document.querySelector("#detail");
+    detail.style.visibility = "visible";
     detail.src = obj;
 }
 
 function baseImg() {
     detail = document.querySelector("#detail");
-    detail.src = "img/base_detail.png";
+    detail.src = "img/transparente.png";
+    detail.style.visibility = "hidden";
 }
 
 function setMap(mapName) {
-    detail = document.querySelector("#detail");
-    detail.style.visibility = "visible";
     map = document.querySelector("#map");
     map.src = "img/" + mapName + "/" + mapName + ".png";
     map.setAttribute('usemap', "#image-map-"+mapName);
