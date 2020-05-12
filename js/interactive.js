0ff5147b18572461a5f7b7df42fcaa3f721ec244
lgem <23lgem@gmail.com>
2020-05-12 13:52:03 -0300

    downsize images

--- js/interactive.js
+++ js/interactive.js
@@ -5,5 +5,13 @@ function change(obj) {
 
 function baseImg() {
     detail = document.querySelector("#detail");
-    detail.src = "img/base.png";
+    detail.src = "img/base_detail.png";
+}
+
+function setMap(mapName) {
+    detail = document.querySelector("#detail");
+    detail.style.visibility = "visible";
+    map = document.querySelector("#map");
+    map.src = "img/" + mapName + "/" + mapName + ".png";
+    map.setAttribute('usemap', "#image-map-"+mapName);
 }
