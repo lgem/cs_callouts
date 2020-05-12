e56f28381407fb622ed0091a3d3ac123992df475
lgem <23lgem@gmail.com>
2020-05-12 12:54:50 -0300

    inferno as index

---
+++ js/interactive.js
@@ -0,0 +1,9 @@
+function change(obj) {
+    detail = document.querySelector("#detail");
+    detail.src = obj;
+}
+
+function baseImg() {
+    detail = document.querySelector("#detail");
+    detail.src = "img/base.png";
+}
